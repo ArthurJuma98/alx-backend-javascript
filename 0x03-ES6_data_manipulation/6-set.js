e243608eaf2fd061a1dad03eca4f58255143769d
ArthurJuma98 <arthurjuma98@gmail.com>
2023-07-31 16:08:01 +0000

    ES6 Data Manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Arthur Ochilo <https://github.com/ArthurJuma98>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
